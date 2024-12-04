f65df4e38a679f572443dd172dbe355c92fb8bec
Théau.D (L'alternant) <theau@form-dev.fr>
2024-12-04 14:33:18 +0100

    🔨 feat(QrCode): Fix no route found and url decode corruption of payload
    Refs: #325

--- src/Controller/QrCodeController.php
+++ src/Controller/QrCodeController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route(path: '/qr-code')]
 class QrCodeController extends AbstractController
 {
 
@@ -23,30 +24,42 @@ class QrCodeController extends AbstractController
     }
 
 
-    #[Route('/qr-code/{encryption}', name: 'app_qr_code', methods: ['GET'])]
+    #[Route('', methods: ['GET'])]
     public function index(Request $request, SecurityService $securityService): Response
     {
-        $decoded = urldecode(urldecode($request->get('encryption')));
+        $encoded = $request->query->get('token');
+        if (!$encoded) {
+            return $this->render('exception/error.html.twig', [
+                'code' => '400',
+                'message' => 'Le paramètre d\'encryption est manquant ou invalide.',
+            ]);
+        }
+
         try {
+            // Double décodage
+            $decoded = rawurldecode($encoded);
             $payload = $securityService->decodeAndDecrypt($decoded, 'AES-256-CBC');
         } catch (Exception $e) {
             return $this->render('exception/error.html.twig', [
-                'error' => 'Le message recu est corrumpu'
+                'code' => '500',
+                'message' => 'Le message reçu est corrompu ou invalide : ' . $e->getMessage(),
             ]);
         }
         //call API to get the data related to the payload
         $data = json_decode($payload, true);
 
         $isV2 = true;
-        $url = $_SERVER['APP_SERV_V2'] . '/api/external/questionnaires/' . urlencode(urlencode($decoded)) . '/learners';
+        $url = $_SERVER['APP_SERV_V2'] . '/api/external/questionnaires?token=' . rawurlencode($decoded);
         $response = $this->client->request(
             'GET',
             $url
         );
+
+
         $response = $response->toArray(false);
         if (!isset($response['message'])) {
             return $this->render('exception/error.html.twig', [
-                'code' => 500,
+                'code' => '500',
                 'message' => 'Récupération du questionnaire impossible'
             ]);
         }
@@ -62,26 +75,26 @@ class QrCodeController extends AbstractController
         }
 
         return $this->render('qr_code/index.html.twig', [
-            'encryption' => $request->get('encryption'),
+            'encryption' => $encoded,
             'interns' => $interns,
         ]);
     }
 
-    #[Route('/qr-code/{encryption}/redirect', name: 'app_qr_code_questionary', methods: ['POST'])]
+    #[Route('/redirect', name: 'app_qr_code_questionary', methods: ['POST'])]
     public function redirectToQuestionary(Request $request): Response
     {
         $selectedInternDataJson = $request->request->get('selectedInternData');
         $selectedIntern = json_decode($selectedInternDataJson, true);
 
         if (!$selectedIntern) {
-            return $this->render('error_custom.html.twig', [
+            return $this->render('error.html.twig', [
                 'code' => '500',
                 'message' => 'Erreur : Les données du stagiaire sélectionné sont introuvables.'
             ]);
         }
 
         if (!isset($selectedIntern['url'])) {
-            return $this->render('error_custom.html.twig', [
+            return $this->render('error.html.twig', [
                 'code' => '500',
                 'message' => 'Erreur : Impossible de récupérer l\'URL du questionnaire pour le stagiaire sélectionné.'
             ]);
